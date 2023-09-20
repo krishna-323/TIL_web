b5f0cb4c4e22c682d97c286388a3f4d4004bb07e
Krishna <krishna.reddy@ikyam.com>
2023-09-20 13:08:11 +0530

    forms filds data.

--- lib/forms/form_template.dart
+++ lib/forms/form_template.dart
@@ -20,44 +20,13 @@ class _TemplateScreenState extends State<TemplateScreen> {
 
   List titleDes = [
     {
-      "title": 'Template Title 1',
-      'description': 'Template description 1',
+      "title": '',
+      'description': '',
+      "fieldListData":[]
     },
   ];
 
-  List templateData =[
-    // [
-    //   {
-    //     "type": "textField",
-    //     "question": " Enter your Name",
-    //     "answer": "",
-    //     "isHover": false
-    //   },
-    //   {
-    //     "type": "textField",
-    //     "question": "Enter Age",
-    //     "answer": "",
-    //     "isHover": false
-    //   },
-    //   {
-    //     "type": "date",
-    //     "question": "Enter Date of birth",
-    //     "answer": "",
-    //     "isHover": false
-    //   },
-    //   {
-    //     "type": "test",
-    //     "question": "Select applicable choice",
-    //     "length": 2,
-    //     "answer": "",
-    //     "dropdownAnswer": [
-    //       "Choice 1",
-    //       "Choice 2"
-    //     ],
-    //     "isHover": false
-    //   }
-    // ],
-  ];
+
 
 
 
@@ -108,34 +77,13 @@ class _TemplateScreenState extends State<TemplateScreen> {
                                                 titleDes.add(  {
                                                   "title": value['title'],
                                                   'description': value['description'],
+                                                  "fieldListData":value['fieldsList']
                                                 },);
-                                                templateData.add(value['fieldsList']);
+                                               // templateData.add(value['fieldsList']);
                                               }
                                             });
                                           }
                                         });
-
-                                        // Navigator.push(context,
-                                        //     PageRouteBuilder(
-                                        //       maintainState: false,
-                                        //       pageBuilder: (context, animation, secondaryAnimation) => const AddNewTemplate(),
-                                        //     )).then((value) {
-                                        //   if(value==null){
-                                        //
-                                        //   }
-                                        //   else{
-                                        //     setState(() {
-                                        //       if(value['fieldsList'].isNotEmpty){
-                                        //         titleDes.add(  {
-                                        //           "title": value['title'],
-                                        //           'description': value['description'],
-                                        //         },);
-                                        //         templateData.add(value['fieldsList']);
-                                        //       }
-                                        //     });
-                                        //   }
-                                        // });
-
                                 },
                               ),
                             ),
@@ -148,7 +96,7 @@ class _TemplateScreenState extends State<TemplateScreen> {
                     padding: const EdgeInsets.all(48.0),
                     child: Wrap(
                       children: [
-                        for(int i=0;i<templateData.length;i++)
+                        for(int i=1;i<titleDes.length;i++)
                           Padding(
                             padding: const EdgeInsets.all(24.0),
                             child: ElevatedButton(
@@ -161,102 +109,16 @@ class _TemplateScreenState extends State<TemplateScreen> {
                                   Navigator.push(context, PageRouteBuilder(maintainState: false,pageBuilder: (context, animation1, animation2) => PreviewScreen(
                                       selectedDestination: widget.args.selectedDestination,
                                       drawerWidth: widget.args.drawerWidth,
-                                      title:titleDes[i]['title'],description:titleDes[i]['description'],fieldsList: templateData[i]),));
-
+                                      title:titleDes[i]['title'],description:titleDes[i]['description'],
+                                      fieldsList: titleDes[i]["fieldListData"],
+                                    //fieldsList: templateData[i]),
+                                  )));
                                 },
-                                child:  SizedBox(height: 150,width: 140,child: Center(child: Text("Template ${i+1}")))),
+                                child:  SizedBox(height: 150,width: 140,child: Center(child:
+                                    Text(titleDes[i]["title"]??"")
+                                //Text("Template ${i+1}")
+                                ))),
                           ),
-
-                        // Padding(
-                        //   padding: const EdgeInsets.all(24.0),
-                        //   child: ElevatedButton(
-                        //       style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        //           const RoundedRectangleBorder(
-                        //             borderRadius: BorderRadius.zero,
-                        //           )
-                        //       )),
-                        //       onPressed: () {
-                        //         List tempData =[
-                        //           {
-                        //             "type": "textField",
-                        //             "question": "Enter Your Restaurants Name",
-                        //             "answer": "",
-                        //             "isHover": false
-                        //           },
-                        //           {
-                        //             "type": "rating",
-                        //             "question": "Enter Rating",
-                        //             "answer": "",
-                        //             "isHover": false
-                        //           },
-                        //           {
-                        //             "type": "date",
-                        //             "question": "Select Date of Visit",
-                        //             "answer": "",
-                        //             "isHover": false
-                        //           },
-                        //           {
-                        //             "type": "radio",
-                        //             "question": " Select Yes or No",
-                        //             "answer": "YesOrNo.no",
-                        //             "isHover": false
-                        //           }
-                        //         ];
-                        //         Navigator.push(context, PageRouteBuilder(maintainState: false,pageBuilder: (context, animation, secondaryAnimation) => PreviewScreen(fieldsList: tempData, title: '',description: '',),));
-                        //
-                        //       },
-                        //       child: const SizedBox(height: 150,width: 140,child: Center(child: Text("Template 2")))),
-                        // ),
-                        //
-                        // Padding(
-                        //   padding: const EdgeInsets.all(24.0),
-                        //   child: ElevatedButton(
-                        //       style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        //           const RoundedRectangleBorder(
-                        //             borderRadius: BorderRadius.zero,
-                        //           )
-                        //       )),
-                        //       onPressed: () {  },
-                        //       child: const SizedBox(height: 150,width: 140,child: Center(child: Text("Template 3")))),
-                        // ),
-                        //
-                        // Padding(
-                        //   padding: const EdgeInsets.all(24.0),
-                        //   child: ElevatedButton(
-                        //       style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        //           const RoundedRectangleBorder(
-                        //             borderRadius: BorderRadius.zero,
-                        //           )
-                        //       )),
-                        //       onPressed: () {  },
-                        //       child: const SizedBox(height: 150,width: 140,child: Center(child: Text("Template 4")))),
-                        // ),
-                        //
-                        // Padding(
-                        //   padding: const EdgeInsets.all(24.0),
-                        //   child: ElevatedButton(
-                        //       style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        //           const RoundedRectangleBorder(
-                        //             borderRadius: BorderRadius.zero,
-                        //           )
-                        //       )),
-                        //       onPressed: () {  },
-                        //       child: const SizedBox(height: 150,width: 140,child: Center(child: Text("Template 5")))),
-                        // ),
-                        //
-                        // Padding(
-                        //   padding: const EdgeInsets.all(24.0),
-                        //   child: ElevatedButton(
-                        //       style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        //           const RoundedRectangleBorder(
-                        //             borderRadius: BorderRadius.zero,
-                        //           )
-                        //       )),
-                        //       onPressed: () {  },
-                        //       child: const SizedBox(height: 150,width: 140,child: Center(child: Text("Template 6")))),
-                        // ),
-
-
                       ],
                     ),
                   ),
--- lib/forms/preview.dart
+++ lib/forms/preview.dart
@@ -1,5 +1,8 @@
 import 'dart:developer';
+import 'dart:typed_data';
 
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:intl/intl.dart';
@@ -66,21 +69,11 @@ class _PreviewScreenState extends State<PreviewScreen> {
                         child: AppBar(
                             elevation: 4,
                             automaticallyImplyLeading: true,
-                            title: Row(
+                            title: const Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                const Text('Dynamic Form',),
-                                SizedBox(width: 100,
-                                  height: 30,
-                                  child: OutlinedMButton(text: 'Save',
-                                      borderColor: mSaveButton,
-                                      buttonColor: mSaveButton,
-                                      textColor: Colors.white,
-                                      onTap:(){
-                                        if (_formKey.currentState!.validate()) {}
-                                      }
-                                  ),
-                                ),
+                                Text('Dynamic Form',),
+
                               ],
                             ))),
                     Padding(
@@ -160,6 +153,7 @@ class _PreviewScreenState extends State<PreviewScreen> {
                                                   }
                                                 },
                                                 focusNode: myFocusNode[index],
+                                                titleName: titleController.text,
                                                 //callback: fieldsList[index] ,
                                               ),
                                             ),
@@ -186,30 +180,48 @@ class _PreviewScreenState extends State<PreviewScreen> {
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: SizedBox(width: 100,
-          height: 30,
-          child: OutlinedMButton(text: 'Cancel',
-              borderColor: mSaveButton,
-              buttonColor: mSaveButton,
-              textColor: Colors.white,
-              onTap:(){
-                if (_formKey.currentState!.validate()) {
-                  if(fieldsList.isNotEmpty) {
-                    Navigator.pop(context);
-                    // Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) => PreviewScreen(fieldsList: fieldsList),));
+        child: Row(mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(width: 100,
+              height: 30,
+              child: OutlinedMButton(text: 'Save',
+                  borderColor: mSaveButton,
+                  buttonColor: mSaveButton,
+                  textColor: Colors.white,
+                  onTap:(){
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.of(context).pop();
+                    }
                   }
-                }
-              }
-          ),
+              ),
+            ),
+            const SizedBox(width: 50,),
+            SizedBox(width: 100,
+              height: 30,
+              child: OutlinedMButton(text: 'Cancel',
+                  borderColor: mSaveButton,
+                  buttonColor: mSaveButton,
+                  textColor: Colors.white,
+                  onTap:(){
+                    if (_formKey.currentState!.validate()) {
+                      if(fieldsList.isNotEmpty) {
+                        Navigator.pop(context);
+                        // Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) => PreviewScreen(fieldsList: fieldsList),));
+                      }
+                    }
+                  }
+              ),
+            ),
+          ],
         ),
       ),
-
     );
   }
 }
 
 
 class DynamicFields extends StatefulWidget {
+  final String titleName;
   final Map initialValue;
   final Function(Map) callback;
   final FocusNode focusNode;
@@ -218,6 +230,7 @@ class DynamicFields extends StatefulWidget {
     super.key,
     required this.initialValue,
     required this.callback, required this.focusNode,
+    required this.titleName,
   });
 
   @override
@@ -228,10 +241,153 @@ class _DynamicFieldsState extends State<DynamicFields> {
   late final TextEditingController questionController;
   late final TextEditingController answerController;
   late FocusNode myFocusNode;
+  final storageRef= FirebaseStorage.instance.ref();
+  var storeFileName= TextEditingController();
+  List fullPathList=[];
 
+ // File Picker Async Function.
+  Future filePicker(questionName) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      Uint8List? fileBytes = result.files.first.bytes;
+      String fileName = result.files.first.name;
+      if(fileName.endsWith("xlsx") || fileName.endsWith("docx") || fileName.endsWith("pdf")){
+        // Static Folder Name.
+        // await storageRef.child("files/$fileName").putData(fileBytes!);
+
+        //This Dynamic Folder Name Based On Form Name.
+         await storageRef.child('${widget.titleName}/$fileName').putData(fileBytes!);
+
+
+        //This Dynamic Folder based on Form Name and File Question Name.
+       // await storageRef.child(widget.titleName).child("$questionName/$fileName").putData(fileBytes!);
+
+      }
+      else{
+        selectFilesShouldBeXlsxAndDocx();
+      }
+    }
+  }
+  selectFilesShouldBeXlsxAndDocx(){
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: StatefulBuilder(
+            builder: (context, setState) {
+              return SizedBox(
+                height: 220,
+                width: 300,
+                child: Stack(children: [
+                  Container(
+                    decoration: BoxDecoration( color: Colors.white,borderRadius: BorderRadius.circular(5)),
+                    margin:const EdgeInsets.only(top: 13.0,right: 8.0),
+                    child: Padding(
+                      padding:  const EdgeInsets.only(left: 20.0,right: 25),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Icon(
+                            Icons.warning_rounded,
+                            color: Colors.red,
+                            size: 50,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Center(
+                              child: Text(
+                                'Selected Files Must Be Xlsx Or Docx',
+                                style: TextStyle(
+                                    color: Colors.indigo,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15),
+                              )),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          MaterialButton(
+                              color: Colors.blue,
+                              child: const Text("Ok",style: TextStyle(color: Colors.white),),
+                              onPressed: (){
+                                Navigator.of(context).pop();
+                              }),
+                          const SizedBox(height: 20,),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(right: 0.0,
+
+                    child: InkWell(
+                      child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              border: Border.all(
+                                color:
+                                const Color.fromRGBO(204, 204, 204, 1),
+                              ),
+                              color: Colors.blue),
+                          child: const Icon(
+                            Icons.close_sharp,
+                            color: Colors.white,
+                          )),
+                      onTap: () {
+                        setState(() {
+                          Navigator.of(context).pop();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+  //Displaying Uploaded Files UI.
+  Future<void> displayingUploadedFilesInUI(questionIndex) async {
+    //Getting based On Form name.
+    ListResult store=await storageRef.child("${widget.titleName}/").listAll();
+    //Form Name and Index Number.
+    // final fileData1 =await storageRef.child("${widget.titleName}/$questionIndex/").listAll();
+    for(int fullPath=0;fullPath<store.items.length;fullPath++){
+      setState((){
+        // File Names Adding To Empty List.
+        fullPathList.add(store.items[fullPath].fullPath);
+      });
+    }
+  }
+
+  //Delete File.
+  Future<void> deleteFile(String url) async {
+    try {
+      // Create a reference to the file to delete.
+      await storageRef.child(url).delete();
+      storeFileName.text="";
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('File Deleted')));
+      setState(() {
+        displayingUploadedFilesInUI("");
+        fullPathList=[];
+      });
+    } catch (e) {
+      log("Error deleting db from cloud: $e");
+    }
+  }
   @override
   void initState() {
     super.initState();
+    if(widget.titleName.isNotEmpty){
+      displayingUploadedFilesInUI("");
+    }
     myFocusNode = widget.focusNode;
     try{
       questionController = TextEditingController();
@@ -341,10 +497,10 @@ class _DynamicFieldsState extends State<DynamicFields> {
               },
 
               decoration: const InputDecoration(hintText: "Answer"),
-              // validator: (v) {
-              //   if (v == null || v.trim().isEmpty) return 'Please enter something';
-              //   return null;
-              // },
+              validator: (v) {
+                if (v == null || v.trim().isEmpty) return 'Please enter something';
+                return null;
+              },
             ),
           ],
         );
@@ -473,10 +629,10 @@ class _DynamicFieldsState extends State<DynamicFields> {
               },
 
               decoration: const InputDecoration(hintText: "Answer"),
-              // validator: (v) {
-              //   if (v == null || v.trim().isEmpty) return 'Please enter something';
-              //   return null;
-              // },
+              validator: (v) {
+                if (v == null || v.trim().isEmpty) return 'Please enter something';
+                return null;
+              },
             ),
           ],
         );
@@ -578,6 +734,100 @@ class _DynamicFieldsState extends State<DynamicFields> {
                   });
                 },
               ),
+            ),
+          ],
+        );
+      case 'fileType':
+        return  Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              focusNode: myFocusNode,
+              controller: questionController,
+              onChanged:(value){
+                setState(() {
+                  widget.callback({
+                    'type':widget.initialValue['type'],
+                    "question":questionController.text,
+                    "answer":answerController.text
+                  });
+                });
+              },
+              decoration: const InputDecoration(hintText: "Files Types?"),
+              validator: (v) {
+                if (v == null || v.trim().isEmpty) {
+                  myFocusNode.requestFocus();
+                  return 'Please enter something';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 50,),
+            Row(crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MaterialButton(
+                    color: Colors.blue,
+                    child: const Text("Upload Files",style: TextStyle(color: Colors.white),),
+                    onPressed: (){
+                     filePicker(widget.initialValue['question']).whenComplete(() {
+                        setState(() {
+                          displayingUploadedFilesInUI(widget.initialValue['question']);
+                          fullPathList=[];
+                        });
+                     });
+
+                    }),
+                const SizedBox(width: 50,),
+                fullPathList.isNotEmpty?
+                Expanded(
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context,int index){
+                        if(index<fullPathList.length){
+                          return
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Text(fullPathList[index].replaceAll("${widget.titleName}/", "")),
+                                ),
+                                const SizedBox(width: 20,),
+                                MaterialButton(
+                                  color: Colors.blue,
+                                  child: const Text('Delete',style: TextStyle(color: Colors.white),),
+                                  onPressed: (){
+                                    deleteFile(fullPathList[index]);
+                                  },),
+                                const SizedBox(width: 20,),
+
+                                // MaterialButton(
+                                //   color: Colors.blue,
+                                //     child: Text("Download",style: TextStyle(color: Colors.white),),
+                                //     onPressed: (){
+                                //
+                                // })
+                              ],
+                            );
+                        }
+                        return null;
+                      }),
+                ):
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: "Upload Files"),
+                    controller: storeFileName,
+                    validator: (v) {
+                      if (v == null || v.trim().isEmpty) {
+                        myFocusNode.requestFocus();
+                        return 'Upload  docx Or Xlsx Files';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(width: 30,),
+
+              ],
             ),
           ],
         );
@@ -717,17 +967,19 @@ class _SelectDropListState extends State<SelectDropList> with SingleTickerProvid
                   children: [
                     Row(
                       children: <Widget>[
-                        Center(
-                          child: Container(
-                            padding: const EdgeInsets.only(left: 16.0,right: 10,top: 10,bottom: 0),
-                            child: Text(items[i].title,
-                                style: const TextStyle(
-                                    color: Color(0xFF000000),
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 14),
-                                maxLines: 3,
-                                textAlign: TextAlign.start,
-                                overflow: TextOverflow.ellipsis),
+                        Flexible(
+                          child: Center(
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 16.0,right: 10,top: 10,bottom: 0),
+                              child: Text(items[i].title,
+                                  style: const TextStyle(
+                                      color: Color(0xFF000000),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14),
+                                  maxLines: 3,
+                                  textAlign: TextAlign.start,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
                           ),
                         ),
                       ],
