941c9964552be47597fe9542bb0d2ac281637e3e
Sathya <mssathyaprakash03@gmail.com>
2026-04-20 07:51:06 +0530

    Flutter version

--- lib/classes/env.dart
+++ lib/classes/env.dart
@@ -1,3 +1,3 @@
 abstract class StaticData{
-  static String url = "http://ec2-3-108-114-3.ap-south-1.compute.amazonaws.com:8080/til-0.0.1/api/";
+  static String url = "https://6gnvztgt-8080.inc1.devtunnels.ms/api/";
 }
